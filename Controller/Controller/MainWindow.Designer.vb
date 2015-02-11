2951ea798673b6dffb118f7a37780bbca7bcbb4e
Wim <wdalof@hotmail.com>
2015-02-11 16:59:04 +0100

    Controller test.

---
+++ Controller/Controller/MainWindow.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainWindow
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainWindow))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.statusListBox = New System.Windows.Forms.ListBox()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButtonControllerStart = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButtonControllerStop = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.LoadFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(542, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadFileToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'statusListBox
+        '
+        Me.statusListBox.FormattingEnabled = True
+        Me.statusListBox.Location = New System.Drawing.Point(12, 53)
+        Me.statusListBox.Name = "statusListBox"
+        Me.statusListBox.Size = New System.Drawing.Size(518, 316)
+        Me.statusListBox.TabIndex = 1
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButtonControllerStart, Me.ToolStripButtonControllerStop})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(542, 25)
+        Me.ToolStrip1.TabIndex = 2
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButtonControllerStart
+        '
+        Me.ToolStripButtonControllerStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButtonControllerStart.Image = CType(resources.GetObject("ToolStripButtonControllerStart.Image"), System.Drawing.Image)
+        Me.ToolStripButtonControllerStart.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButtonControllerStart.Name = "ToolStripButtonControllerStart"
+        Me.ToolStripButtonControllerStart.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButtonControllerStart.Text = "Start controller"
+        '
+        'ToolStripButtonControllerStop
+        '
+        Me.ToolStripButtonControllerStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButtonControllerStop.Image = CType(resources.GetObject("ToolStripButtonControllerStop.Image"), System.Drawing.Image)
+        Me.ToolStripButtonControllerStop.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButtonControllerStop.Name = "ToolStripButtonControllerStop"
+        Me.ToolStripButtonControllerStop.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButtonControllerStop.Text = "Stop controller"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'LoadFileToolStripMenuItem
+        '
+        Me.LoadFileToolStripMenuItem.Name = "LoadFileToolStripMenuItem"
+        Me.LoadFileToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.LoadFileToolStripMenuItem.Text = "Load File..."
+        '
+        'MainWindow
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(542, 377)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.statusListBox)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "MainWindow"
+        Me.Text = "Controller"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents statusListBox As System.Windows.Forms.ListBox
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents ToolStripButtonControllerStart As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripButtonControllerStop As System.Windows.Forms.ToolStripButton
+    Friend WithEvents LoadFileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+
+End Class
---
+++ Controller/Controller/MainWindow.vb
@@ -0,0 +1,19 @@
+﻿Public Class MainWindow
+    Dim controller As Controller
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        System.Windows.Forms.Application.Exit()
+    End Sub
+
+    Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        controller = New Controller(Me)
+    End Sub
+
+    Private Sub ToolStripButtonControllerStart_Click(sender As Object, e As EventArgs) Handles ToolStripButtonControllerStart.Click
+        controller.StartController()
+    End Sub
+
+    Private Sub ToolStripButtonControllerStop_Click(sender As Object, e As EventArgs) Handles ToolStripButtonControllerStop.Click
+        controller.StopController()
+    End Sub
+End Class
