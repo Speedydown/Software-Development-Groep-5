9e23464234c715fd7778017e46c05302d8611e13
wdalof <wdalof@hotmail.com>
2015-05-31 18:47:28 +0200

    Laatste versie (final). Zie bescrijving.
    - Er verschijnt nu een waarschuwing wanneer er torrent software is
    gedetecteerd.
    - Busbanen functioneren nu correct.
    - Het is nu mogelijk automatisch voertuigen te spawn (zie tabblad "Test"
    in de applicatie).
    - Code toegevoegd.
    - Andere verbeteringen/fixes.

---
+++ Controller/Controller/Server.vb
@@ -0,0 +1,183 @@
+﻿Imports System.ComponentModel
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+
+Public Module Server
+    Public Property CancelPending As Byte
+    Private _serverThread As Thread
+    Private _connectedClient As ConnectedClient
+    Private _clientSocket As TcpClient
+    Private _threadStarted As Byte
+    Private _mainWindow As MainWindow
+
+    Public Sub StartServer()
+
+        If _serverThread Is Nothing Then
+            If My.Settings.AutoClearLog Then
+                _mainWindow.ClearLog()
+            End If
+
+            If (Process.GetProcessesByName("uTorrent").Length > 0 Or Process.GetProcessesByName("BitTorrent").Length > 0) Then
+                _mainWindow.LogMessage(6, "Torrent software is running!")
+            End If
+
+            'Create a new thread.
+            _serverThread = New Thread(AddressOf ServerWorker)
+            _serverThread.IsBackground = True
+            _serverThread.Start()
+        Else
+            _mainWindow.LogMessage(1, "Server is already started.")
+        End If
+    End Sub
+
+    Public Sub StopServer()
+
+        'Stop the controller if it started.
+        If _serverThread IsNot Nothing AndAlso _serverThread.IsAlive() Then
+
+            If Not CancelPending Then
+
+                'Close the client socket, if a simulator is connected.
+                If _clientSocket IsNot Nothing Then
+                    _clientSocket.Close()
+
+                    _clientSocket = Nothing
+
+                    _mainWindow.LogMessage(1, "Client socket closed.")
+                End If
+
+                'Remove the client from the server.
+                If _connectedClient IsNot Nothing Then
+                    _connectedClient = Nothing
+
+                    _mainWindow.LogMessage(1, "Client removed from server.")
+                End If
+
+                'Stop the thread.
+                Thread.VolatileWrite(_threadStarted, 0)
+
+                CancelPending = True
+            Else
+                _mainWindow.LogMessage(1, "Cancel is pending. Please wait.")
+
+            End If
+        Else
+            _mainWindow.LogMessage(1, "Server has not been started.")
+        End If
+    End Sub
+
+    'Thread worker.
+    Private Sub ServerWorker()
+        Thread.VolatileWrite(_threadStarted, 1)
+
+        _mainWindow.LogMessage(1, "Starting server...")
+
+        Try
+            'Listen for the simulator to connect.
+            Dim tcpListener As New TcpListener(IPAddress.Any, My.Settings.ListeningPortNumber)
+            tcpListener.Start()
+
+            _mainWindow.LogMessage(1, "Server is listening on " + _mainWindow.GetIpAddress() + ":" + My.Settings.ListeningPortNumber.ToString + "...")
+            _mainWindow.LogMessage(1, "Waiting for simulator to connect...")
+
+            While Thread.VolatileRead(_threadStarted) = 1
+
+                'Sleep to avoid locking up the CPU.
+                Thread.Sleep(10)
+
+                'Check if there is a connection pending.
+                If tcpListener.Pending Then
+                    If _connectedClient Is Nothing Then
+                        'Accept the connection.
+                        _clientSocket = tcpListener.AcceptTcpClient()
+
+                        _mainWindow.LogMessage(1, "Client connected.")
+
+                        'Start the controller.
+                        Controller.StartController()
+
+                        'Create a new client object.
+                        _connectedClient = New ConnectedClient(_clientSocket, _mainWindow)
+                    End If
+                End If
+            End While
+
+            'Stop the controller when the server stops.
+            If Controller.CheckControllerStarted() Then
+                _mainWindow.LogMessage(1, "Stopping controller.")
+
+                Controller.StopController()
+
+                If Controller.GetThread() IsNot Nothing Then
+                    If Controller.GetThread().IsAlive Then
+                        Controller.GetThread.Join()
+                    End If
+                End If
+            End If
+
+            tcpListener.Stop()
+
+            _mainWindow.LogMessage(1, "Server stopped.")
+            CancelPending = False
+
+            _serverThread = Nothing
+
+        Catch exception As Exception
+            _mainWindow.LogMessage(2, exception.Message)
+        End Try
+    End Sub
+
+    'Pass a incomming message to the controller.
+    Public Sub IncomingMessage(ByVal rawMessage As Byte())
+
+        Dim message As Message = New Message(rawMessage(0), New Integer() {rawMessage(1), rawMessage(2), rawMessage(3)})
+
+        Controller.ReceiveMessage(message)
+
+        'Add the message to the log.
+        _mainWindow.LogMessage(4, message.MessageToString() + ".")
+    End Sub
+
+    Public Sub OutgoingMessage(ByVal message As Message)
+
+        'Check if client is connected before sending a message.
+        If _connectedClient IsNot Nothing Then
+            If _connectedClient.ClientSocket().Connected() Then
+
+                'Send a message to the simulator.
+                _connectedClient.SendMessage(message)
+                _mainWindow.LogMessage(3, message.MessageToString() + ".")
+            End If
+        End If
+    End Sub
+
+    'Sets an reference to the main window.
+    Public Sub SetMainWindow(ByVal mainWindow As MainWindow)
+
+        _mainWindow = mainWindow
+    End Sub
+
+    'Çheck if the client is connected.
+    Public Function CheckClientConnected() As Boolean
+
+        If (_connectedClient IsNot Nothing) Then
+            Return True
+        End If
+
+        Return False
+    End Function
+
+    'Check if the server is started.
+    Public Function CheckServerStarted() As Boolean
+        If _serverThread IsNot Nothing Then
+            If (_serverThread.IsAlive) Then
+                Return True
+            End If
+        End If
+
+        Return False
+    End Function
+
+End Module
---
+++ Controller/Controller/TrafficLight.vb
@@ -0,0 +1,55 @@
+﻿Imports System.ComponentModel
+Imports System.Net.Sockets
+Imports System.Threading
+
+'Traffic light object that contains information.
+Public Class TrafficLight
+    Public Property State As Integer
+    Public Property Sequence As Integer
+    Public Property Id As Integer
+    Public Property Type As Integer
+    Private _queue As New Integer
+
+    Sub New(ByVal id As Integer, ByVal type As Integer)
+        Me.Id = id
+        Me.Type = type
+    End Sub
+
+    'Add a vehicle to the queue.
+    Public Sub AddVehicle()
+        _queue = _queue + 1
+    End Sub
+
+    'Remove a vehicle from the queue.
+    Public Sub RemoveVehicle()
+        _queue = _queue - 1
+    End Sub
+
+    'Get queue size.
+    Public Function GetVehicleCount() As Integer
+        Return _queue
+    End Function
+
+    'Reset queue.
+    Public Sub ResetVehicleCount()
+        _queue = 0
+    End Sub
+
+    'Change traffic light state to green.
+    Public Sub ChangeStateToGreen()
+        Controller.SendMessage(New Message(2, New Integer() {Id, 2, 0}))
+        State = 2
+    End Sub
+
+    'Change traffic light state to orange.
+    Public Sub ChangeStateToOrange()
+        Controller.SendMessage(New Message(2, New Integer() {Id, 1, 0}))
+        State = 1
+    End Sub
+
+    'Change traffic light state to red.
+    Public Sub ChangeStateToRed()
+        Controller.SendMessage(New Message(2, New Integer() {Id, 0, 0}))
+        State = 0
+    End Sub
+End Class
